d6e2071b583915021c11958ea534f460c3ee99f9
Michael Lee <michaelhjnlee@gmail.com>
2021-11-15 12:10:31 -0800

    skeleton made

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import Navbar from './Navbar';
+import Section from './Section';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      
+      <Navbar />
+      <Section id="1" />
+      <Section id="2" />
+      <Section id="3" />
+      <Section id="4" />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Navbar.jsx
@@ -0,0 +1,35 @@
+
+
+const Navbar = () => {
+  return (
+    <div className="Navbar">
+      <div className="NavBarMain">
+        Michael Lee's Pave Take Home
+      </div>
+      <div className="NavItemContainer">
+        <div className="NavBarItem">
+          <a href="#1">
+          Item 1
+          </a>
+        </div>
+        <div className="NavBarItem">
+        <a href="#2">
+          Item 2
+          </a>
+        </div>
+        <div className="NavBarItem">
+        <a href="#3">
+          Item 3
+          </a>
+        </div>
+        <div className="NavBarItem">
+        <a href="#4">
+          Item 4
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar;
